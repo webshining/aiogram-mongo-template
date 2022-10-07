4382ac7aa4878e69e55bf3a95820c3062ba93240
WebShining <artemilienkono@gmail.com>
2022-10-07 13:40:41 +0300

    add default commands

---
+++ app/commands/default.py
@@ -0,0 +1,20 @@
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+from loader import _, bot, i18n
+
+
+def get_default_commands(lang: str = 'en') -> list[BotCommand]:
+    commands = [
+        BotCommand('/start', _('start bot', locale=lang)),
+        BotCommand('/lang', _('change language', locale=lang)),
+    ]
+
+    return commands
+
+
+async def set_default_commands():
+    await bot.set_my_commands(get_default_commands(), scope=BotCommandScopeDefault())
+
+    for lang in i18n.available_locales:
+        await bot.set_my_commands(get_default_commands(lang), scope=BotCommandScopeDefault(), language_code=lang)
+        
