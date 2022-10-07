745af8e82c97812974c45014891ef2857d46357a
WebShining <artemilienkono@gmail.com>
2022-10-07 12:33:54 +0300

    Update poetry template

--- app/middlewares/user.py
+++ app/middlewares/user.py
@@ -1,12 +1,15 @@
 from aiogram.types import Message, CallbackQuery
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from database import get_or_create_user
+from utils import logger
 
 
 class UserMiddleware(BaseMiddleware):
     @staticmethod
     async def on_process_message(message: Message, data: dict):
         from_user = message.from_user
+        
+        logger.debug
 
         data['user'] = get_or_create_user(from_user.id, from_user.full_name, from_user.username)
 
