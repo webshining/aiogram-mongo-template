0375b8aff75f8133e3164397833fdd43269d27f6
WebShining <artemilienkono@gmail.com>
2022-10-07 12:46:12 +0300

    Add inline mod

--- app/middlewares/user.py
+++ app/middlewares/user.py
@@ -1,20 +1,24 @@
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, InlineQuery
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from database import get_or_create_user
 from utils import logger
 
 
 class UserMiddleware(BaseMiddleware):
-    @staticmethod
-    async def on_process_message(message: Message, data: dict):
+    async def on_process_message(self, message: Message, data: dict):
         from_user = message.from_user
         
-        logger.debug
+        logger.debug()
 
         data['user'] = get_or_create_user(from_user.id, from_user.full_name, from_user.username)
 
-    @staticmethod
-    async def on_callback_query(call: CallbackQuery, data: dict):
+    async def on_callback_query(self, call: CallbackQuery, data: dict):
         from_user = call.from_user
+
+        data['user'] = get_or_create_user(from_user.id, from_user.full_name, from_user.username)
+        
+
+    async def on_process_inline_query(inline_query: InlineQuery, data: dict[str]):
+        from_user = inline_query.from_user
 
         data['user'] = get_or_create_user(from_user.id, from_user.full_name, from_user.username)
