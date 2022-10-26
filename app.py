b584ef38ae24d9edd20bcbff7381d8fc7dfd7d26
WebShining <webshinings@gmail.com>
2022-10-26 19:01:14 +0300

    add default commands

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from app.commands.default import set_default_commands
-from loader import dp, bot
+from aiogram.types import BotCommandScopeDefault 
+from loader import dp, bot, i18n
 
 
 async def on_startup():
     from app.commands import set_default_commands
     await set_default_commands()
     print("Bot started!")
+    
+async def on_shutdown():
+    await bot.delete_my_commands(scope=BotCommandScopeDefault())
+    for lang in i18n.available_locales:
+        await bot.delete_my_commands(scope=BotCommandScopeDefault(), language_code=lang)
+    print("Bot stoped!")
 
 
 if __name__ == '__main__':
     import app.handlers
-    dp.startup.register(set_default_commands())
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     dp.run_polling(bot)
