376980cecc278261e4264944aaebd01550ef8cf4
WebShining <webshinings@gmail.com>
2022-10-26 19:01:31 +0300

    Update translate

--- app/handlers/users/start.py
+++ app/handlers/users/start.py
@@ -2,9 +2,9 @@ from aiogram.types import Message
 from aiogram.filters import Command
 
 
-from loader import dp
+from loader import dp, _
 
 
 @dp.message(Command("start"))
 async def _start(message: Message):
-    await message.answer(f'Hello <b>{message.from_user.full_name}</b>')
+    await message.answer(_('Hello <b>{}</b>').format(message.from_user.full_name))
