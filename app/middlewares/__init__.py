f4635ad4e49c74992e0eef4cda5ca8c87d32c6c2
WebShining <webshining@protonmail.com>
2022-10-28 01:36:55 +0300

    update middleware

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -1,5 +1,7 @@
 from loader import dp
 from .user import UserMiddleware
+from .inter import i18n_middleware
 
 if __name__ == 'app.middlewares':
+    dp.update.middleware(i18n_middleware)
     dp.update.middleware(UserMiddleware())
